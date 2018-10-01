399f381f9d7891c99a79db0856073ee2957d1ef7
Saptarshi Dey <saptarshidey1987@gmail.com>
2018-10-02 00:46:20 +0530

    Added DS and DF Methods

--- src/main/scala/com/saptarshi/sparkscala/examples/WordCount.scala
+++ src/main/scala/com/saptarshi/sparkscala/examples/WordCount.scala
@@ -1,18 +1,28 @@
 package com.saptarshi.sparkscala.examples
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Dataset
 
 object WordCount {
 
     def main(args: Array[String]): Unit = {
-        require(args != null && args.length == 1, "Provide the Input filename")
-        val Array(filename) = args
+        require(args != null && args.length == 2, "Input filename and Method (DS/DF) required")
+        val Array(filename, method) = args
 
         val spark = SparkSession.builder.appName("WordCount").getOrCreate()
         import spark.implicits._
 
-        val lines = spark.read.textFile(filename)
-        val wc = lines.flatMap(_.split(" ")).groupByKey(v => v).count()
+        var wc: Dataset[(String, Long)] = null
+
+        if (method == "DS") {    // Using Spark Dataset
+            val lines = spark.read.text(filename).as[String]
+            wc = lines.flatMap(_.split(" ")).groupByKey(v => v).count()
+        }
+        else {    // Using Spark DataFrame
+            val lines = spark.read.text(filename)
+            wc = lines.flatMap(_.getString(0).split(" ")).groupByKey(v => v).count()
+        }
+
         wc.collect.foreach(println)
     }
 
