fc2f7d37c6e1a138618c37821587e236eebff620
Saptarshi Dey <saptarshidey1987@gmail.com>
2018-10-02 12:39:37 +0530

    Add DS/DF/RDD Methods

--- src/main/scala/com/saptarshi/sparkscala/examples/WordCount.scala
+++ src/main/scala/com/saptarshi/sparkscala/examples/WordCount.scala
@@ -6,24 +6,29 @@ import org.apache.spark.sql.Dataset
 object WordCount {
 
     def main(args: Array[String]): Unit = {
-        require(args != null && args.length == 2, "Input filename and Method (DS/DF) required")
+        require(args != null && args.length == 2, "Input filename and Method (DS/DF/RDD) required")
         val Array(filename, method) = args
 
         val spark = SparkSession.builder.appName("WordCount").getOrCreate()
         import spark.implicits._
 
-        var wc: Dataset[(String, Long)] = null
-
         if (method == "DS") {    // Using Spark Dataset
             val lines = spark.read.text(filename).as[String]
-            wc = lines.flatMap(_.split(" ")).groupByKey(v => v).count()
+            val wc = lines.flatMap(_.split(" ")).groupByKey(v => v).count()
+            wc.collect.foreach(println)
         }
-        else {    // Using Spark DataFrame
+        else if (method == "DF") {    // Using Spark DataFrame
             val lines = spark.read.text(filename)
-            wc = lines.flatMap(_.getString(0).split(" ")).groupByKey(v => v).count()
+            val wc = lines.flatMap(_.getString(0).split(" ")).groupByKey(v => v).count()
+            wc.collect.foreach(println)
+        }
+        else {    // Using Spark RDD
+            val lines = spark.read.text(filename).as[String].rdd
+            val wc = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+            wc.collect.foreach(println)
         }
 
-        wc.collect.foreach(println)
+        spark.stop()
     }
 
 }
